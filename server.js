a18f94c92433cb12e49b883a570c8ff63f8f732a
NikhilSuryanarayanan <n4nikhilkana@gmail.com>
2024-01-29 00:46:02 +0530

    pushmaster2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; // You can change this port number as needed
 
 app.get('/', (req, res) => {
-  res.send('App deployed, Hi to DevOps!');
+  res.send('nikhildone deployed, Hi to DevOps!');
 });
 
 app.listen(port, () => {
