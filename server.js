5d4004cde73dae4f9b4dbe674309153e3fee921c
NikhilSuryanarayanan <n4nikhilkana@gmail.com>
2024-02-02 11:17:21 +0530

    deone2 maybe

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000; // You can change this port number as needed
 
 app.get('/', (req, res) => {
-  res.send('pushed boy deployed, Hi to DevOps!');
+  res.send('boy deployed, Hi to DevOps!');
 });
 
 app.listen(port, () => {
